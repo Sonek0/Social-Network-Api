0938377419729ce13e97e071d0e5409e6ef082bc
jacob <jacob.7050@gmail.com>
2023-04-27 13:43:58 -0700

    Express and Mongoose npm i

--- Routes/index.js
+++ Routes/index.js
@@ -1,8 +1,12 @@
+
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
+//add prefix to all api routes
 router.use('/api', apiRoutes);
 
+//Wrong route used
 router.use((req, res) => res.send('Wrong route!'));
 
+//Module exports
 module.exports = router;
