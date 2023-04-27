fc17fc5a945df90eef0de86cf0d80e228a58511d
jacob <jacob.7050@gmail.com>
2023-04-27 14:56:26 -0700

    user router get/post/delete routes

---
+++ index.js
@@ -0,0 +1,11 @@
+// Require express and mongoose
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./Routes');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
